bfa2bb58e3d3f86543757df90bd234c0da1814e6
Davi Luis <daviluis323@gmail.com>
2019-04-22 17:28:38 -0300

    Create Page

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView
+from .models import Post
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'core/home.html'
