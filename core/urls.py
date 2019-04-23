916489cb6d81e4e9c67da6392c41b804746a3d91
Davi Luis <daviluis323@gmail.com>
2019-04-23 08:33:30 -0300

    Upload Page

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomeView
+from .views import HomeView, UploadView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('upload/', UploadView.upload_image, name='upload'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
+from .forms import PostForm
 from .models import Post
 
 class HomeView(ListView):
     model = Post
     template_name = 'core/home.html'
+
+class UploadView(TemplateView):
+    template_name = 'core/upload.html'
+
+    def upload_image(request):
+        if request.method == 'POST':
+            form = PostForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                return redirect('home')
+        else:
+            form = PostForm()
+        return render(request, 'core/upload.html', {
+            'form': form
+        })
